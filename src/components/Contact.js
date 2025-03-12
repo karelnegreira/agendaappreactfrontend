0a6151bd0c4feb0e7fd5dad0a6b6d82d05c1d429
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-03-13 02:01:00 +1200

    Contact.js component coded

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,29 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Contact = () => {
+const Contact = ({ contact }) => {
   return (
-    <a className='contact_item'>
+    <Link to={`/contacts/${contact.id}`} className='contact_item'>
         <div className="contact_header">
             <div className="contact_image">
-                <img src='' alt='' />
+                <img src={contact.photoUrl} alt={contact.name} />
             </div>
             <div className='contact_details'>
-                <p className='contact_name'>Karel Negreira</p>
-                <p className='contact_title'>unknown</p>
+                <p className='contact_name'>{contact.name.substring(0, 15)}</p>
+                <p className='contact_title'>{contact.title}</p>
             </div>
             <div className='contact_body'>
-                <p><i className='bi bi-envelope'></i>email</p>
-                <p><i className='bi bi-geo'></i>address</p>
-                <p><i className='bi bi-telephone'></i>phone</p>
+                <p><i className='bi bi-envelope'></i>{contact.email.substring(0, 20)}</p>
+                <p><i className='bi bi-geo'></i>{contact.address}</p>
+                <p><i className='bi bi-telephone'></i>{contact.phone}</p>
+                <p>{contact.status === 'Active' 
+                    ? <i className='bi bi-check-circle'></i> : 
+                    <i className="bi bi-x-circle"></i>}{contact.status}
+                </p>
             </div>
         </div>
-    </a>
+    </Link>
   )
 }
 
